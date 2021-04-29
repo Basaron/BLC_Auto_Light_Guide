1f3d05071b572fd7ff295765ec3734d86fe33c46
Biorrith <201909298@post.au.dk>
2021-04-29 14:41:02 +0200

    Made proper upload when getting a JSON event, updated the data base tables so they work

--- Database and webserver/BLC_database.js
+++ Database and webserver/BLC_database.js
@@ -1,38 +1,19 @@
+//TODO: Throw errors to make the code not crash when uploading
+
+//TODO: consider making the functionality to register a profile?
+
+//TODO: tighten up the code
+
+//TODO: Make a readme on how to setup MySQL
+
+//TODO: make comments
+
 //Load the server using express
+var mysql = require('mysql'); 
+var mqtt = require('mqtt'); //For connecting to the broker
+var client = mqtt.connect('mqtt:localhost:1883') //Connect to the local broker on standard port
 
-/*To create tables: 
-CREATE TABLE `blc`.`data` (
-	`event_id` INT KEY AUTO_INCREMENT NOT NULL, 
-	`event` VARCHAR(255), 
-	`timestamp` timestamp NOT NULL, 
-	FOREIGN KEY `UsrID` REFERENCES users(user_id)
-	FOREIGN KEY `sensorID REFERENCS devices(device_id)
-	);
-
-MAYBE ADD TO TIMESTAMP: TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-CREATE TABLE `blc`.`devices` (
-	`device_id` INT KEY AUTO_INCREMENT NOT NULL,
-	`device_type` VARCHAR(30) NOT NULL, 
-	`device_location` VARCHAR (30) NOT NULL
-	);
-
-
-CREATE TABLE `blc`.`users` (
-	`user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT, 
-	`username` VARCHAR(64) NOT NULL, 
-	`psw` VARCHAR(64) NOT NULL, 
-	PRIMARY KEY (`user_id`),
-	UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
-*/
-
-
-const express = require('express')
-var app = express()
-var mysql = require('mysql'); //https://www.npmjs.com/package/mysql
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt:localhost:1883')
-
+//Creating the mySQL connection for the database.
 var connection = mysql.createConnection({
 	multipleStatements: true,
     port: '3306',
@@ -42,17 +23,15 @@ var connection = mysql.createConnection({
 	database: "BLC"
 });
 
-app.listen(3000, () => {
-    console.log("Test!")
-})
-
+//If no broker is found
 client.on('close', (err) => {
 	console.log("Looking for broker")
 })
 
+//If connected to the broker
 client.on('connect', (err) => {
 	console.log("Connected, trying to subscribe")
-	client.subscribe(Topic, mqtt_subscribe);
+	client.subscribe(Topic, mqtt_subscribe); //Subscribe
 })
 
 function mqtt_subscribe(err, granted) {
@@ -60,127 +39,72 @@ function mqtt_subscribe(err, granted) {
     if (err) {console.log(err);}
 };
 
-var Topic = 'server/#'; //subscribe to all topics
 
+var Topic = 'server/#'; //subscribe to all topics in server
 
+//If a message is recieved, go to the function that handles it
 client.on('message', mqtt_messsageReceived);
 
+//For handling a recieved message
 function mqtt_messsageReceived(topic, message, packet) {
 	
-	//const obj = JSON.parse(message.toString())
-	//console.log(obj.event)
-	
-	
-	var message_str = message.toString(); //convert byte array to string
-	console.log(message_str)
-	insert_message(topic, message_str)
-	
-	
-	//message_str = message_str.replace(/\n$/, ''); //remove new line
+	//Get the send data into JSON
+	const obj = JSON.parse(message) //.toString()
 
-	//payload syntax: clientID,topic,message
-	/*if (countInstances(message_str) != 1) {
+
+	//If it recieves something empty, its an invalid payload.
+		//Maybe consider if it should be when it recieves something of different length then
+		//expected.	
+	if (Object.keys(obj).length === 0) { 
 		console.log("Invalid payload");
-		} else {	
-		//insert_message(topic, message_str, packet);
-		console.log(message_arr);
-	}*/
+		
+	}
+	//If not, split the data into variables.
+	else {
+		var description = obj.description
+		var start_time = obj.timestamp	
+		var visit_length = obj.length
+		var user_id = obj.patientId
+		var sensor_id = obj.sensorId
+		var time_to_bathroom = obj.toBathroom
+		var time_from_bathroom = obj.fromBathroom
+	
+		//For viewing/developing
+		console.log(description)
+		console.log("Start time = ", start_time)
+		console.log("Time on bathroom in seconds = ", visit_length)
+		console.log("To bathroom: ", time_to_bathroom)
+		console.log("Patient ID = ", user_id)
+		console.log("Sensor ID = ", sensor_id)
+		
+		//Insert the data
+		insert_message(description, start_time, time_to_bathroom, visit_length, time_from_bathroom, sensor_id, user_id)
+	}	
 };
 
-
 //insert a row into the tbl_messages table
-function insert_message(topic, message_str) { //topic, message_str, packet
+function insert_message(description, start_time, time_to_bathroom, visit_length, time_from_bathroom, sensor_id, user_id) { //topic, message_str, packet
 	
-	var sql = "INSERT INTO presentation (topic, msg) VALUES (?, ?)"
+	//Make the query.
+	var query = "INSERT INTO data (dscription, start_time, time_to_bathroom, time_on_bathroom, time_from_bathroom, device_id, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)"
 
-	connection.query(sql, [topic, message_str], (err) =>{
+	connection.query(query, [description, start_time, time_to_bathroom, visit_length, time_from_bathroom, sensor_id, user_id], (err) =>{
 		if (err){
 		  console.log(err);
 		}
 		else console.log("Inserted successfully.");
 	});
-
-	/*var message_arr = extract_string(message_str); //split a string into an array
-	var clientID= message_arr[0];
-	var message = message_arr[1];
-	var sql = "INSERT INTO ?? (??,??,??) VALUES (?,?,?)";
-	var params = ['tbl_messages', 'clientID', 'topic', 'message', clientID, topic, message];
-	sql = mysql.format(sql, params);	
-	
-	connection.query(sql, function (error, results) {
-		if (error) throw error;
-		console.log("Message added: " + message_str);
-	}); */
 };
-
-
-/*
-client.on('connect', mqtt_connect);
-client.on('reconnect', mqtt_reconnect);
-client.on('error', mqtt_error);
-client.on('close', mqtt_close);
-
-function mqtt_connect() {
-    console.log("Connecting MQTT");
-    client.subscribe(Topic, mqtt_subscribe);
-};
-
-
-
-
-function mqtt_reconnect(err) {
-    //console.log("Reconnect MQTT");
-    //if (err) {console.log(err);}
-	client  = mqtt.connect(Broker_URL, options);
-};
-
-function mqtt_error(err) {
-    //console.log("Error!");
-	//if (err) {console.log(err);}
-};
-
-function after_publish() {
-	//do nothing
-};
-*/
-
-//receive a message from MQTT broker
-
-
-
-
-
-
-/*
-function mqtt_close() {
-	//console.log("Close MQTT");
-};
-
-////////////////////////////////////////////////////
-///////////////////// MYSQL ////////////////////////
-////////////////////////////////////////////////////
-
-
 
 connection.connect(function(err) {
 	if (err) throw err;
 	console.log("Database Connected!");
 });
 
-	
 
-//split a string into an array of substrings
-function extract_string(message_str) {
-	var message_arr = message_str.split(","); //convert to array	
-	return message_arr;
-};	
+//Done TODO:
+//// TODO: Make the JSON object into different variables to upload
 
-*/
-//count number of delimiters in a string
-var delimiter = ",";
-function countInstances(message_str) {
-	var substrings = message_str.split(delimiter);
-	return substrings.length - 1;
-};
+//TODO: Upload properly
 
-
+//TODO: Finish the tables so that they follow the 2'nd normal
--- Database and webserver/BLC_webserver.js
+++ Database and webserver/BLC_webserver.js
@@ -1,6 +1,21 @@
+//TODO: Make sessions using cookies
+
+//TODO: Implement the login system
+
+//TODO: Make a proper website
+
+//TODO: Get data down from the database with foreign keys: https://dba.stackexchange.com/questions/129023/selecting-data-from-another-table-using-a-foreign-key
+
+//TODO: Make comments
+
+//TODO: Tighten up the code
+
+
 const express = require('express')
 var app = express()
 app.set('view engine', 'ejs');
+
+
 
 
 app.listen(3000, () => {
