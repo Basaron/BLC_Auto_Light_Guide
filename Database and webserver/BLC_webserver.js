5a6b1f326adac1bc7da80639e0d60c208f7465d3
Biorrith <201909298@post.au.dk>
2021-04-30 12:28:12 +0200

    Made a login system

--- Database and webserver/BLC_webserver.js
+++ Database and webserver/BLC_webserver.js
@@ -10,23 +10,15 @@
 
 //TODO: Tighten up the code
 
+//Libraries
+const express = require('express') //For setting up the website
+const bodyParser = require('body-parser') //For getting the input data
 
-const express = require('express')
+var mysql = require('mysql') //Connecting to the database
 var app = express()
-app.set('view engine', 'ejs');
 
-
-
-
-app.listen(3000, () => {
-    console.log("Test!")
-})
-
-app.get('/', (req, res) => {
-	res.render('test');
-})
-
-var mysql = require('mysql');
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended:true}))
 
 var connection = mysql.createConnection({
 	multipleStatements: true,
@@ -36,5 +28,55 @@ var connection = mysql.createConnection({
 	password: "BLC",
 	database: "BLC"
 });
+
+
+app.listen(3000, () => {
+    console.log("Server up and running")
+})
+
+app.get('/', (req, res) => {
+	res.render('login');
+})
+
+app.post('/login', (req, res) => {
+	var username = req.body.username
+	var password = req.body.password
+	//TODO: make hash of password
+	var query = 'SELECT * FROM users WHERE username = ? AND psw = ?'
+	
+	connection.query(query, [username, password], (err, result) => {
+		if (err){
+			console.log(err);
+		  }
+		else{
+			if(result.length > 0) {
+				var user_id = result[0].user_id
+				console.log(user_id)
+				res.redirect('/homepage')			
+			}
+			else{
+				console.log("Wrong username or password")
+				res.redirect('/')
+			}
+		}
+	})
+})
+
+app.get('/logout', (req, res) => {
+	res.redirect('/');
+  })
+
+  
+app.get('/homepage', (req, res) => {
+	res.render('homepage');
+})
+
+
+function createHash(string_input){
+	const hash = crypto.createHash('sha256')
+	hash.update(string_input);
+	return hash.digest('hex');
+  }
+
 
 
