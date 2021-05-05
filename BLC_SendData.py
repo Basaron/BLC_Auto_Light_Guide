6f4de1a38538976518e6fa9440024004e23cddb1
Biorrith <60431319+Biorrith@users.noreply.github.com>
2021-05-05 12:32:18 +0200

    Update BLC_SendData.py

--- BLC_SendData.py
+++ BLC_SendData.py
@@ -14,32 +14,60 @@ from paho.mqtt import publish
 
 
 class publisher:
-    #def __init__(self):
-    #    return
-
-    def publishData(self, msg: str, val: bool, patient_id: str, start_time, visit_length):
+    def publishData_main(self, patient_id, session_id, to_bathroom, visit_length, from_bathroom, msg):
         event = HEUCOD.HeucodEvent()
-        event.description = msg
-        event.timestamp = start_time #datetime.now().strftime("%D:%T")
-        event.length = visit_length
-        event.to_bathroom = 12
-        event.from_bathroom = 14
-
         event.patient_id = patient_id
-        event.sensor_id = 1
+        event.value1 = session_id
+        event.timestamp = datetime.now().strftime("%D %T") #Format: date - current time
+        event.value2 = to_bathroom
+        event.length = visit_length        
+        event.value3 = from_bathroom
         
-        
-
-
-        print(event.to_json())
+        #Stuff not used in our database
+        event.description = msg
+        event.event_type = HEUCOD.HeucodEventType.BedOccupancyEvent
 
         publish.single(hostname="192.168.1.2",
                         port = 1883,
-                        topic=f"server/test", 
+                        topic=f"server/main_table", 
                         payload = event.to_json())
-   
+
+
+    def publishData_dump(self, patient_id, session_id, device_id, msg):
+        event = HEUCOD.HeucodEvent()
+
+        event.patient_id = patient_id
+        event.value1 = session_id
+        event.timestamp = datetime.now().strftime("%D %T")        
+        event.sensor_id = 1
+        event.description = msg
+
+        #Stuff not used in our database
+        event.event_type = HEUCOD.HeucodEventType.RoomMovementEvent
+        print(event.to_json())
+        
+        
+        publish.single(hostname="192.168.1.2",
+                        port = 1883,
+                        topic=f"server/dump_table", 
+                        payload = event.to_json())
+        
+
 if __name__ == "__main__":
     pub = publisher()
+    #                    user  session  device  msg
+    pub.publishData_dump(2,    1,       1,      "Movement detected")
+    """
+    pub.publishData_dump(1,    1,       2,      "Movement detected")
+    pub.publishData_dump(1,    1,       3,      "Movement detected")
+    pub.publishData_dump(1,    1,       4,      "Movement detected")
+    pub.publishData_dump(1,    1,       5,      "Movement detected")
+    pub.publishData_dump(1,    1,       4,      "Movement detected")
+    pub.publishData_dump(1,    1,       3,      "Movement detected")
+    pub.publishData_dump(1,    1,       2,      "Movement detected")
+    pub.publishData_dump(1,    1,       1,      "Movement detected")
 
-    pub.publishData("User went to bathroom", True, 1, datetime.now().strftime("%D %T"), 73)
+    #                    patient session to_bathroom visit len from_bathroom, msg
+    pub.publishData_main(1,      1,      15,         10,       15,            "User went to bathroom and back to bed")
+    """
     
